ed4cb425c9e03b8419657b3c629338fbe3277ce2
royl8 <royl8@qq.com>
2018-08-14 16:16:18 +0800

    init commit

---
+++ sc_player.py
@@ -0,0 +1,13 @@
+import sc2
+from sc2 import run_game, maps, Race, Difficulty
+from sc2.player import Bot, Computer
+
+class SentdeBot(sc2.BotAI):
+    async def on_step(self, iteration):
+        await self.distribute_workers()
+
+
+run_game(maps.get('Simple64'), [
+    Bot(Race.Protoss, SentdeBot()),
+    Computer(Race.Terran, Difficulty.Easy)
+], realtime=True)
